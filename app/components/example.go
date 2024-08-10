7811b8d198ac7f8bc2f9a443d1e4c3c9100f2a32
Nathan fiscaletti <nate.fiscaletti@gmail.com>
2024-08-10 16:17:54 -0500

    Updates

--- app/components/example.go
+++ app/components/example.go
@@ -3,15 +3,29 @@ package components
 import (
 	"github.com/hexops/vecty"
 	"github.com/hexops/vecty/elem"
+	"github.com/hexops/vecty/event"
 )
 
 // ExampleComponent is a simple Vecty component.
 type ExampleComponent struct {
 	vecty.Core
+
+	text string
+}
+
+func (c *ExampleComponent) onClick(event *vecty.Event) {
+	c.text = "Hello, world!"
+	vecty.Rerender(c)
 }
 
 func (c *ExampleComponent) Render() vecty.ComponentOrHTML {
 	return elem.Body(
-		vecty.Text("Hello from Vecty in WebAssembly!"),
+		elem.Button(
+			vecty.Text("Click me!"),
+			vecty.Markup(
+				event.Click(c.onClick),
+			),
+		),
+		vecty.Text(c.text),
 	)
 }
